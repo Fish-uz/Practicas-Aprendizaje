e61a69c83f36d811264fc6b774143b2d0b186e8a
Frankuz <136502968+Fish-uz@users.noreply.github.com>
2026-03-24 14:33:08 -0400

    Delete EjercicioCarro directory

--- EjercicioCarro/Auto.java
+++
@@ -1,9 +0,0 @@
-package EjercicioCarro;
-public class Auto extends Vehiculo {
-    private boolean esNuevo;
-
-    public Auto(String marca, String modelo, double precio, boolean esNuevo) {
-        super(marca, modelo, precio); // <--- Llama al constructor de Vehiculo
-        this.esNuevo = esNuevo;
-    }
-}
